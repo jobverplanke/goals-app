d36ca5ad2705772f8ac6d09c115c848d4b321254
Job Verplanke <job.verplanke@gmail.com>
2020-11-18 02:48:00 +0100

    Restructured Controller

--- src/Domain/Objective/Http/Controllers/Api/ObjectiveController.php
+++ src/Domain/Objective/Http/Controllers/Api/ObjectiveController.php
@@ -5,7 +5,8 @@ declare(strict_types=1);
 namespace Domain\Objective\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Domain\Objective\Actions\CreateObjectiveAction;
+use Domain\Objective\Actions\Objective\CreateObjectiveAction;
+use Domain\Objective\Http\Requests\Objective\StoreObjectiveRequest;
 use Domain\Objective\Http\Resources\Objective\ObjectiveResource;
 use Domain\Objective\Services\Objective\ObjectiveService;
 use Illuminate\Http\Request;
@@ -13,6 +14,7 @@ use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 /**
  * Class ObjectiveController
+ *
  * @author Job Verplanke <job.verplanke@gmail.com>
  */
 class ObjectiveController extends Controller
@@ -37,10 +39,10 @@ class ObjectiveController extends Controller
         );
     }
 
-    public function store(Request $request, CreateObjectiveAction $createObjectiveAction): ObjectiveResource
+    public function store(StoreObjectiveRequest $request, CreateObjectiveAction $createObjectiveAction): ObjectiveResource
     {
         return new ObjectiveResource(
-            $createObjectiveAction->execute($request)
+            $createObjectiveAction->execute($request->validated(), true)
         );
     }
 
@@ -59,9 +61,9 @@ class ObjectiveController extends Controller
      * @param \Illuminate\Http\Request $request
      * @param $id
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id)
     {
-        //
+        $this->objectiveService->getRepository()->update($id);
     }
 
     /**
